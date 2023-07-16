faf8271c5c1f6965908b82e05249472813ce0d40
Meyzan16 <Meyzan1605@gmail.com>
2023-07-16 19:34:41 +0700

    test vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,11 @@
-import type { InferGetServerSidePropsType } from "next";
-import React from "react";
-import { ProjectInterface } from "@common.types";
-import { fetchAllProjects } from "@lib/actions";
-import ProjectCard from "@components/ProjectCard";
-import Categories from "@components/Categories";
-import LoadMore from "@components/LoadMore";
+import React from 'react'
 
-type ProjectsSearch = {
-    projectSearch: {
-        edges: {node: ProjectInterface}[];
-        pageInfo: {
-            hasPreviousPage: boolean;
-            hasNextPage: boolean;
-            startCursor: string;
-            endCursor: string;
-        }
-    }
+const page = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
 }
 
-type SearchParams = {
-    category?: string ;
-    endcursor?: string ;
-}
-
-type Props = {
-    searchParams: SearchParams;
-}
-
-export const dynamic = 'force-dynamic';
-export const dynamicParams = true;
-export const revalidate = 0;
-
-const Home = async ({searchParams: { category,endcursor }}: Props) => {
-
-            const data = await fetchAllProjects(category,endcursor) as ProjectsSearch;
-
-            const projectsToDisplay = data?.projectSearch?.edges || [];
-        
-            if(projectsToDisplay.length === 0) {
-                
-                return (
-                    <section className="flexStart flex-col paddings">
-                        <Categories />
-        
-                        <p className="no-result-text text-center">
-                            No anime collection found, go create some first.
-                        </p>
-        
-                    </section>
-                )
-        
-            }
-
-            const pagination = data?.projectSearch?.pageInfo;
-
-            console.log(projectsToDisplay);
-
-    return (
-        <section className="flex-start flex-col paddings mb-16">
-            <Categories />
-
-            <section className="projects-grid">
-                {
-                    projectsToDisplay.map(
-                        ({node}: {node: ProjectInterface}) => 
-                    (
-                        <ProjectCard 
-                            key={node?.id}
-                            id={node?.id}
-                            image={node?.image}
-                            genre={node?.genre}
-                            episode={node?.episode}
-                            title={node?.title}
-                            name={node?.createdBy?.name}
-                            avatarUrl={node?.createdBy?.avatarUrl}
-                            userId={node?.createdBy?.id}
-
-                        />
-                    )
-                    )
-                }
-
-            </section>
-
-            <LoadMore 
-                startCursor={pagination.startCursor}
-                endCursor={pagination.endCursor}
-                hasPreviousPage={pagination.hasPreviousPage}
-                hasNextPage={pagination.hasNextPage}
-
-
-            />
-
-        </section>
-    )
-}
-
-export default Home;
+export default page
