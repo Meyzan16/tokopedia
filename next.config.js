58268e124fc66dafecc6964d723606aef72d6b67
Meyzan16 <Meyzan1605@gmail.com>
2023-07-16 17:58:45 +0700

    perbarui config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
+    },
+    webpack(config) {
+        config.experiments = {
+          ...config.experiments,
+          topLevelAwait: true,
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
