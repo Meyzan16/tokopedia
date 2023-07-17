b315d67759449b27cf24dc2c3d6bd047c739ac3f
Meyzan16 <Meyzan1605@gmail.com>
2023-07-17 12:03:29 +0700

    perbaruii constanst

--- constants/index.ts
+++ constants/index.ts
@@ -1,10 +1,4 @@
-// export const NavLinks = [
-//     { href: '/', key: 'Inspiration', text: 'Inspiration' },
-//     { href: '/', key: 'Find Projects', text: 'Find Projects' },
-//     { href: '/', key: 'Learn Development', text: 'Learn Development' },
-//     { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
-//     { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
-// ];
+
   
   export const categoryFilters = [
     "One Piece",
